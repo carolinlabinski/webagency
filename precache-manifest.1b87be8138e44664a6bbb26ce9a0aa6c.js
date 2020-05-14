73a2d1752d7dc6ce55cdd4e9ab9cbdac5cf02587
carolin.labinski <carolin.labinski@yahoo.de>
2020-05-14 23:08:48 +0200

    Updates

---
+++ precache-manifest.1b87be8138e44664a6bbb26ce9a0aa6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c429d432d66944d46c22ab7d63cda6c3",
+    "url": "/webagency/index.html"
+  },
+  {
+    "revision": "9683e0f51344abb2c7dc",
+    "url": "/webagency/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8a2d7e6935e0dc190cfb",
+    "url": "/webagency/static/css/main.7cdecf55.chunk.css"
+  },
+  {
+    "revision": "9683e0f51344abb2c7dc",
+    "url": "/webagency/static/js/2.0da346df.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/webagency/static/js/2.0da346df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2d7e6935e0dc190cfb",
+    "url": "/webagency/static/js/main.e79ec456.chunk.js"
+  },
+  {
+    "revision": "29b7f55842d980ba1f52",
+    "url": "/webagency/static/js/runtime-main.c7efed43.js"
+  }
+]);
